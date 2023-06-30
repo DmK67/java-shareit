94dc2069038e05ac7e40217ed14c808e5c2b76c2
Dmitriy K <79517037815@yandex.ru>
2023-06-30 18:54:05 +0300

    Сontinuation TZ-14 ver: 1.61 30.06

---
+++ src/main/java/ru/practicum/shareit/exceptions/CustomErrorResponse.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+public class CustomErrorResponse extends ResponseEntityExceptionHandler {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+
+}
