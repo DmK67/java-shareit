b43501ef7d3f9e12b656a52be363333439ad012a
Dmitriy K <79517037815@yandex.ru>
2023-08-02 11:12:17 +0300

    TZ-15 ver. 2.1 02.08.23

--- src/main/java/ru/practicum/shareit/item/comment/mapper/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/comment/mapper/CommentMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.comment.mapper;
 
-import lombok.Builder;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.comment.dto.CommentDto;
 import ru.practicum.shareit.item.comment.model.Comment;
